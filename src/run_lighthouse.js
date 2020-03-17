605b245c07f025fe77aacf2e32f88fac8d8fd6ee
Anders Windelhed <anders.windelhed@gmail.com>
2020-03-17 21:36:16 +0100

    Closing broser if there is an exception in lighthouse

--- src/run_lighthouse.js
+++ src/run_lighthouse.js
@@ -2,20 +2,27 @@ const puppeteer = require('puppeteer')
 const lighthouse = require('lighthouse')
 
 const runLighthouse = async (url) => {
-  const browser = await puppeteer.launch({
+  const browserOptions = {
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  })
+  }
+  const browser = await puppeteer.launch(browserOptions)
 
-  const res = await lighthouse(url, {
-    port: new URL(browser.wsEndpoint()).port,
-    output: 'json',
-    onlyCategories: ['performance'],
-    onlyAudits: [],
-  })
+  try {
+    const lighthouseOptions = {
+      port: new URL(browser.wsEndpoint()).port,
+      output: 'json',
+      onlyCategories: ['performance'],
+      onlyAudits: [],
+    }
+    const res = await lighthouse(url, lighthouseOptions)
 
-  browser.close()
-  return res
+    browser.close()
+    return res
+  } catch (e) {
+    browser.close()
+    throw e
+  }
 }
 
 module.exports = runLighthouse
